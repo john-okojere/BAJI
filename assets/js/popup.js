bfd31890cba5d72eca509c38af35f84267426c3a
john-okojere <johnokojere08@gmail.com>
2024-02-24 13:17:49 +0100

    modified:   assets/css/animate.css 	modified:   assets/css/gallery.css 	modified:   assets/js/popup.js 	modified:   gallery.html

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -14,7 +14,7 @@ function openModal() {
   function plusSlides(n) {
     showSlides(slideIndex += n);
   }
-  
+
   // Thumbnail image controls
   function currentSlide(n) {
     showSlides(slideIndex = n);
